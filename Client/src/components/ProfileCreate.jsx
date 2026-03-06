efc9e8b18a1a48753d87b1eb22e80674fb5764cd
LucasSeabraFrada <frada9@rogers.com>
2026-03-06 14:09:28 -0500

    CreateProfile Addition

--- Client/src/components/ProfileCreate.jsx
+++ Client/src/components/ProfileCreate.jsx
@@ -2,36 +2,32 @@ import "./ProfileTest.css";
 import InterestDisplay from "./InterestDisplay.jsx";
 import SkillDisplay from "./SkillDisplay.jsx";
 import { useState } from "react";
-
-function ProfileEdit(){
-    const [user] = useState({id: 1, username: "JohnDoe", email: "johndoe@example.com", firstName: "John", lastName: "Doe", interests: [{id: 1, name: "Hiking"}], location: {country: "Canada", region: "Ontario", city: "Toronto", streetAddress: "123 Main St"}, privacyLevel: 2, employment: {workplace: "Example Corp", position: "Example Position"}, skills: [{id: 1, name: "JavaScript"}, {id: 2, name: "Python"}]});//Hardcoded for testing purposes.
+function ProfileCreate(){
+    const [user] = useState({});//Hardcoded for testing purposes only.
     return(
     <div className="container">
-        <h1>Looking For Love Profile Editor</h1>
-        <h2>Edit Profile</h2>
+        <h1>Looking For Love Profile Creator</h1>
+        <h2>Create Profile</h2>
 
         <form className="form">
 
         <label >Username</label>
-        <input type="text" placeholder="Username" defaultValue={user.username}/>
+        <input type="text" placeholder="Username"/>
 
         <label >Email</label>
-        <input type="text" placeholder="Email" defaultValue={user.email}/>
+        <input type="text" placeholder="Email"/>
 
         <label >Old Password</label>
         <input type="password" placeholder="Password"/>
 
-        <label >New Password</label>
-        <input type="password" placeholder="New Password"/>
-
         <label >Confirm New Password</label>
-        <input type="password" placeholder="Confirm New Password"/>
+        <input type="password" placeholder="Confirm Password"/>
 
         <label >FirstName</label>
-        <input type="text" placeholder="First Name" defaultValue = {user.firstName}/>
+        <input type="text" placeholder="First Name"/>
 
         <label >LastName</label>
-        <input type="text" placeholder="Last Name" defaultValue = {user.lastName}/>
+        <input type="text" placeholder="Last Name"/>
 
         <label >Skills</label>
         {user.skills.map(skill => <SkillDisplay key={skill.id} skill={skill}/>)/*LF: Placeholder for skills display*/}
@@ -45,25 +41,25 @@ function ProfileEdit(){
         {/*LF: Location inputs below. Was thinking of having privacyLevel affect which of these is shown on profile view for other users. For example, if privacyLevel is 0, everything is shown. If it's 1, everything but address is shown, etc.*/}
         {/*LF: Was thinking of making everything but country optional*/}
         <label >Country</label>
-        <input type="text" placeholder="Country" defaultValue={user.location.country}/>
+        <input type="text" placeholder="Country"/>
         <label >Region</label>
-        <input type="text" placeholder="Province/State/Region" defaultValue={user.location.region || ''}/>
+        <input type="text" placeholder="Province/State/Region"/>
         <label >City</label>
-        <input type="text" placeholder="City/Town" defaultValue={user.location.city || ''}/>
+        <input type="text" placeholder="City/Town"/>
         <label >Address</label>
-        <input type="text" placeholder="Street Address" defaultValue={user.location.streetAddress || ''}/>
+        <input type="text" placeholder="Street Address"/>
         
         {/*LF: Workplace and position inputs below. These will likely also be optional and have privacy levels assigned to them in some way.*/}
         <label >Workplace</label>
-        <input type="text" placeholder="Company" defaultValue={user.employment.workplace || ''}/>
+        <input type="text" placeholder="Company"/>
 
         <label >Position</label>
-        <input type="text" placeholder="Job Title / Position" defaultValue={user.employment.position || ''}/>
+        <input type="text" placeholder="Job Title / Position"/>
 
         <label >PrivacyLevel</label>
-        <input type="number" placeholder="0-10" defaultValue={user.privacyLevel || 3}/>
+        <input type="number" placeholder="0-10"/>
 
-        <button type="submit">Save Profile</button>
+        <button type="submit">Create Profile</button>
         <button type="cancel">Cancel</button>
 
         </form>
@@ -73,4 +69,4 @@ function ProfileEdit(){
         //LF: Skills and interests will likely be a map or array of HTML components that the user can add to in a 'edit profile' page.
         //LF: Editing password ask for the old password once then the new password twice.
 }
-export default ProfileEdit;
+export default ProfileCreate;
