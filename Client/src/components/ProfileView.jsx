a3b0a5afea6a97a78b46dee65fde98517d35de1b
LucasSeabraFrada <frada9@rogers.com>
2026-03-06 14:48:48 -0500

    ProfileConcept

--- Client/src/components/ProfileView.jsx
+++ Client/src/components/ProfileView.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 function ProfileView() {
     const [user] = useState({id: 1, username: "JohnDoe", email: "johndoe@example.com", firstName: "John", lastName: "Doe", interests: [{id: 1, name: "Hiking"}], location: {country: "Canada", region: "Ontario", city: "Toronto", streetAddress: "123 Main St"}, privacyLevel: 2, employment: {workplace: "Example Corp", position: "Example Position"}, skills: [{id: 1, name: "JavaScript"}, {id: 2, name: "Python"}]});//Hardcoded for testing purposes.
     //LF: On the server side, if the privacy level is high, the server will only send the data that the said user would want displayed back to the client.
-    //LF: Then we would have checks to account for those null/missing values. For example, if workplace is null we could just indicate to the viewing user that the information is hidden due to privacy settings.
+    //LF: Then we would have checks to account for those null or missing values. For example, if workplace is null we could just indicate to the viewing user that the information is hidden due to privacy settings.
     return (
         <div className="container">
             <h1>Looking For Love Profile View</h1>
