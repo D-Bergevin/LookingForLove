8b4045908066fb69ec525c85cdf15821c619b887
Lucas <Frada9@rogers.com>
2026-04-10 16:18:52 -0400

    Minor bugfix

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,7 +4,6 @@ import Matches from "./components/Matches.jsx";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ProfileEdit from "./components/ProfileEdit";
-import PotentialMatches from "./components/PotentialMatches.jsx";
 import { profile } from "./util/api.js";
 
 // dev components
