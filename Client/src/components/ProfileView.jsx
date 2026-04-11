2c43a2d8f46a5954c8fffad39343c8b9dda6031c
AbdulWasay29 <133403890+AbdulWasay29@users.noreply.github.com>
2026-04-11 22:07:34 +0500

    fixed center align for profile

--- Client/src/components/ProfileView.jsx
+++ Client/src/components/ProfileView.jsx
@@ -3,18 +3,20 @@ function ProfileView({ user }) {
         return <div>Loading...</div>
     }
     return (
-        <div className="container">
+        <div className="matches_container">
             <h1>Looking For Love Profile View</h1>
             <h2>View Profile</h2>
+            <div className="profile-text-center">
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <p>Name: {user.firstName} {user.lastName}</p>
+                <p>Skills: {user.skills.map(skill => skill).join(", ")}</p>
+                <p>Interests: {user.interests.map(interest => interest).join(", ")}</p>
+                <p>Location: {user.location.country}, {user.location.region}, {user.location.city}, {user.location.address}</p>
+                <p>Workplace: {user.employment.workplace}</p>
+                <p>Position: {user.employment.position}</p>
+            </div>
 
-            <p>Username: {user.username}</p>
-            <p>Email: {user.email}</p>
-            <p>Name: {user.firstName} {user.lastName}</p>
-            <p>Skills: {user.skills.map(skill => skill).join(", ")}</p>
-            <p>Interests: {user.interests.map(interest => interest).join(", ")}</p>
-            <p>Location: {user.location.country}, {user.location.region}, {user.location.city}, {user.location.address}</p>
-            <p>Workplace: {user.employment.workplace}</p>
-            <p>Position: {user.employment.position}</p>
         </div>
     );
 }
